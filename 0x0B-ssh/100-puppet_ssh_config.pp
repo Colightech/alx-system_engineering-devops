bf8abbf6f0e0555619f9051f31ab88eb79157477
Colightech <saintchris4@gmail.com>
2024-01-21 15:19:15 -0800

    udating

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,13 +5,13 @@ file {'/etc/ssh/ssh_fonfig':
 	ensure => present,
 }
 
-file_line {'Turn of password auth',
+file_line {'Turn of password auth':
 	path => '/etc/ssh/ssh_config',
 	line => 'PasswordAuthentication',
 	match => '^#PasswordAuthentication',
 }
 
-file_line {'Declare identity file',
+file_line {'Declare identity file':
 	path => '/etc/ssh/ssh_config',
 	line => 'IdentityFile ~/.ssh/school',
 	match => '^#IdentityFile',
